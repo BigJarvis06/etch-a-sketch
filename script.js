151b49e7ddb8d4c278d592409ca2e5884151a9e1
BigJarvis06 <tjdunn06@gmail.com>
2024-12-26 15:41:59 +1100

    added createGrid and clearGrid functions

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
+const container = document.querySelector("#container");
 
+const createGrid = function(height, width) {
+  const numOfSquares = height * width;
+    for (let i = 0; i < numOfSquares; i++) {
+      const square = document.createElement("div");
+      container.appendChild(square);
+      square.classList.add("square");
+    }
+}
+
+const clearGrid = function() {
+  while (container.firstChild) {
+    container.removeChild(container.firstChild)
+  }
+}
